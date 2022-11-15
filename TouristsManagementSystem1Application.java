b65771644e01803c58f780361bb2d9b04a7220ed
SethuMadhavAtConcentrix <118254898+SethuMadhavAtConcentrix@users.noreply.github.com>
2022-11-15 18:17:21 +0530

    Add files via upload

---
+++ TouristsManagementSystem1Application.java
@@ -0,0 +1,13 @@
+package com.touristManagementSystem.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristsManagementSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristsManagementSystem1Application.class, args);
+	}
+
+}
