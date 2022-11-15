21754e57145d6e45c1879d3682887a7b3316c255
gsethu <gsethu@DESKTOP-30KP84U>
2022-11-15 15:55:07 +0530

    2

---
+++ TouristsManagementSystem-1/src/test/java/com/touristManagementSystem/demo/TouristsManagementSystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.touristManagementSystem.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristsManagementSystem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
